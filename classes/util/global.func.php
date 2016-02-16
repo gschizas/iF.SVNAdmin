f038d9c865daf5c6941038fc583cb90b8c571a8f
George Schizas <gschizas@gmail.com>
2016-02-16 20:24:11 +0200

    Handle PHP versions >5.0
    According to current handling, 6.1 is < 5.3

--- classes/util/global.func.php
+++ classes/util/global.func.php
@@ -1,38 +1,37 @@
 <?php
 function checkPHPVersion($minimumVersion)
 {
-  $phpVersion = phpversion();
-  $phpVersionParts = explode(".", $phpVersion);
-  $minVersionParts = explode(".", $minimumVersion);
-  $minVersionPartsCount = count($minVersionParts);
+    $phpVersion = phpversion();
+    $phpVersionParts = explode(".", $phpVersion);
+    $minVersionParts = explode(".", $minimumVersion);
+    $minVersionPartsCount = count($minVersionParts);
 
-  $check = true;
-  if ($minVersionPartsCount >= 1)
-    if ($phpVersionParts[0] < $minVersionParts[0])
-      $check = false;
+    if ($minVersionPartsCount >= 1)
+        if ($phpVersionParts[0] > $minVersionParts[0])
+            return true;
 
-  if ($minVersionPartsCount >= 2)
-    if ($phpVersionParts[1] < $minVersionParts[1])
-      $check = false;
+    if ($minVersionPartsCount >= 2)
+        if ($phpVersionParts[1] > $minVersionParts[1])
+            return true;
 
-  if ($minVersionPartsCount >= 3)
-    if ($phpVersionParts[2] < $minVersionParts[2])
-      $check = false;
+    if ($minVersionPartsCount >= 3)
+        if ($phpVersionParts[2] > $minVersionParts[2])
+            return true;
 
-  return $check;
+    return false;
 }
 
 function exception_handler($exception)
 {
-  echo "<b>Error:</b> ".$exception->getMessage()."<br>";
-  echo "<pre>".$exception->getTraceAsString()."</pre>";
-  exit(1);
+    echo "<b>Error:</b> " . $exception->getMessage() . "<br>";
+    echo "<pre>" . $exception->getTraceAsString() . "</pre>";
+    exit(1);
 }
 
-function tr($text, $args=null)
+function tr($text, $args = null)
 {
-  global $appTR;
-  return $appTR->tr($text, $args);
+    global $appTR;
+    return $appTR->tr($text, $args);
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -41,166 +40,166 @@ function tr($text, $args=null)
 
 function if_log_debug($message)
 {
-	error_log($message);
+    error_log($message);
 }
 
 // ------------------------------------------ Global template "print" functions.
 
 function GlobalHeader()
 {
-	include_once("pages/global-header.php");
+    include_once("pages/global-header.php");
 }
 
 function GlobalFooter()
 {
-	include_once("pages/global-footer.php");
+    include_once("pages/global-footer.php");
 }
 
 function ProcessTemplate($file)
 {
-	include_once("pages/".$file);
+    include_once("pages/" . $file);
 }
 
 function AppVersion()
 {
-	global $appEngine;
-	print($appEngine->getAppVersionString());
+    global $appEngine;
+    print($appEngine->getAppVersionString());
 }
 
 function SessionUsername()
 {
 	if (isset($_SESSION["svnadmin_username"]))
 	{
-		print($_SESSION["svnadmin_username"]);
-	}
+        print($_SESSION["svnadmin_username"]);
+    }
 }
 
 function ScriptName()
 {
-	print(currentScriptFileName());
+    print(currentScriptFileName());
 }
 
 function PrintApplicationVersion()
 {
-	global $appEngine;
-	print($appEngine->getAppVersionString());
+    global $appEngine;
+    print($appEngine->getAppVersionString());
 }
 
 function PrintSessionUsername()
 {
 	if (isset($_SESSION["svnadmin_username"]))
 	{
-		print($_SESSION["svnadmin_username"]);
-	}
+        print($_SESSION["svnadmin_username"]);
+    }
 }
 
 function PrintCurrentScriptName()
 {
-	print(currentScriptFileName());
+    print(currentScriptFileName());
 }
 
 // -------------------------------------------------- Global template functions.
 
 function AppEngine()
 {
-	global $appEngine;
-	return $appEngine;
+    global $appEngine;
+    return $appEngine;
 }
 
-function Translate($text, $args=null)
+function Translate($text, $args = null)
 {
-	global $appTR;
-	print($appTR->tr($text, $args));
+    global $appTR;
+    print($appTR->tr($text, $args));
 }
 
 function CurrentLocale()
 {
 	if (isset($_COOKIE["locale"]) && !empty($_COOKIE["locale"]))
 	{
-		return $_COOKIE["locale"];
-	}
-	return "en_US";
+        return $_COOKIE["locale"];
+    }
+    return "en_US";
 }
 
 function SetValue($varName, $varValue)
 {
-	global $appTemplate;
-	$appTemplate->addReplacement($varName, $varValue);
+    global $appTemplate;
+    $appTemplate->addReplacement($varName, $varValue);
 }
 
 function GetValue($varName)
 {
-	global $appTemplate;
+    global $appTemplate;
 	if (isset($appTemplate->m_replacements[$varName]))
 	{
-		$v=$appTemplate->m_replacements[$varName];
+        $v = $appTemplate->m_replacements[$varName];
 		if (!empty($v))
 		{
-			return $v;
-		}
-	}
-	return NULL;
+            return $v;
+        }
+    }
+    return NULL;
 }
 
 function GetArrayValue($varName)
 {
-	$v=NULL;
+    $v = NULL;
 	if (($v=GetValue($varName)) != NULL)
 	{
 		if (is_array($v))
 		{
-			return $v;
+            return $v;
 		}
 		else
 		{
-			return array($v);
-		}
-	}
-	return array();
+            return array($v);
+        }
+    }
+    return array();
 }
 
 function GetStringValue($varName)
 {
-	$v=NULL;
+    $v = NULL;
 	if (($v=GetValue($varName)) != NULL)
 	{
-		return $v;
-	}
-	return "";
+        return $v;
+    }
+    return "";
 }
 
-function GetIntValue($varName, $base=10)
+function GetIntValue($varName, $base = 10)
 {
-	$v=NULL;
+    $v = NULL;
 	if (($v=GetValue($varName)) != NULL)
 	{
 		if (is_int($v))
 		{
-			return $v;
+            return $v;
 		}
 		else
 		{
-			return intval($v, $base);
-		}
-	}
-	return 0;
+            return intval($v, $base);
+        }
+    }
+    return 0;
 }
 
 function GetBoolValue($varName)
 {
-	$v=NULL;
+    $v = NULL;
 	if (($v=GetValue($varName)) != NULL)
 	{
 		if (is_bool($v))
 		{
-			return $v;
+            return $v;
 		}
 		else
 		{
-			return (boolean)$v;
-		}
-	}
-	return false;
+            return (boolean)$v;
+        }
+    }
+    return false;
 }
 
 function PrintStringValue($varName) { print(GetStringValue($varName)); }
@@ -211,48 +210,48 @@ function PrintBoolValue($varName) { print(GetBoolValue($varName)); }
 
 function IsUserLoggedIn()
 {
-	global $appEngine;
-	return $appEngine->checkUserAuthentication(false);
+    global $appEngine;
+    return $appEngine->checkUserAuthentication(false);
 }
 
 function IsViewUpdateable()
 {
-	global $appEngine;
-	return $appEngine->isViewUpdateable();
+    global $appEngine;
+    return $appEngine->isViewUpdateable();
 }
 
 function IsProviderActive($providerTypeId)
 {
-	global $appEngine;
-	return $appEngine->isProviderActive($providerTypeId);
+    global $appEngine;
+    return $appEngine->isProviderActive($providerTypeId);
 }
 
 function HasAccess($module, $action)
 {
-	global $appEngine;
-	return $appEngine->checkUserAccess($module, $action);
+    global $appEngine;
+    return $appEngine->checkUserAccess($module, $action);
 }
 
 function HasAppExceptions()
 {
-	global $appEngine;
-	$a=$appEngine->getExceptions();
+    global $appEngine;
+    $a = $appEngine->getExceptions();
 	if (!empty($a))
 	{
-		return true;
-	}
-	return false;
+        return true;
+    }
+    return false;
 }
 
 function HasAppMessages()
 {
-	global $appEngine;
-	$a=$appEngine->getMessages();
+    global $appEngine;
+    $a = $appEngine->getMessages();
 	if (!empty($a))
 	{
-		return true;
-	}
-	return false;
+        return true;
+    }
+    return false;
 }
 
 // --------------------------------------------------- Complete html components.
@@ -263,12 +262,13 @@ function HasAppMessages()
  * @param string $tableId The HTML-ID of the table.
  * @param int $columnIndex The column index of the content in which is to search.
  */
-function HtmlFilterBox($tableId, $columnIndex=0)
+function HtmlFilterBox($tableId, $columnIndex = 0)
 {?>
-	<div class="datatablesearch">
-		<?php Translate("Filter"); ?>:
+    <div class="datatablesearch">
+        <?php Translate("Filter"); ?>:
 	  <input type="text" class="filterbox" onkeyup="filterDataTable('<?php print($tableId); ?>', <?php print($columnIndex); ?>,this.value);">
-	</div>
-<?php
+    </div>
+    <?php
 }
+
 ?>
